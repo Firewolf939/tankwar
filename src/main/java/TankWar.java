1b7dd34b4a5570150f3ea5a3d5d565669f45a463
kuofox2003 <kuofox2003@hotmail.com>
2020-07-28 21:41:18 +0800

    完成主遊戲畫面

---
+++ src/main/java/TankWar.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class TankWar {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.add(new GameClient(800,600));
+        frame.setTitle("TankWar");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+    }
+}
